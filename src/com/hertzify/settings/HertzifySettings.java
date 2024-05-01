4160858fdaf373606740cd843f218d9a8fdd8650
AnierinB <anierin@evolution-x.org>
2024-05-01 00:49:14 +0000

    Hertzify: Init empty Hertzify
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: xyyx <xyyx@mail.ru>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Change-Id: I00e6c5ddb73a8abe2e2f4229817374db44303fca
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

---
+++ src/com/hertzify/settings/HertzifySettings.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2026 HertzifyOS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.hertzify.settings;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+@SearchIndexable
+public class HertzifySettings extends DashboardFragment {
+
+    private static final String TAG = "HertzifySettings";
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.hertzify_settings;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.HERTZIFY;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.hertzify_settings);
+}
---
+++ src/com/hertzify/settings/TopLevelHertzifySettingsPreferenceController.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2026 HertzifyOS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.hertzify.settings;
+
+import android.content.Context;
+
+import com.android.settings.core.BasePreferenceController;
+
+public class TopLevelHertzifySettingsPreferenceController extends BasePreferenceController {
+
+    public TopLevelHertzifySettingsPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+}
