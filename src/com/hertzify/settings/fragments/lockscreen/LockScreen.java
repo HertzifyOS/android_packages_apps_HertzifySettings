239d67572fefa80e65a07c3d43671ed1157fe6ff
Pranav Vashi <neobuddy89@gmail.com>
2026-03-25 16:23:33 +0530

    Hertzify: Add Smartspace Toggle & OmniJaws Weather pref
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- src/com/hertzify/settings/fragments/lockscreen/LockScreen.java
+++ src/com/hertzify/settings/fragments/lockscreen/LockScreen.java
@@ -14,12 +14,16 @@ import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
+
+import com.android.internal.util.hertzify.OmniJawsClient;
+import com.hertzify.settings.utils.SystemUtils;
 
 import java.util.List;
 
@@ -28,6 +32,12 @@ public class LockScreen extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "LockScreen";
+
+    private static final String KEY_SMARTSPACE = "lockscreen_smartspace_enabled";
+    private static final String KEY_WEATHER = "lockscreen_weather_enabled";
+
+    private SwitchPreferenceCompat mSmartspace;
+    private SwitchPreferenceCompat mWeather;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,13 +48,45 @@ public class LockScreen extends SettingsPreferenceFragment implements
         final ContentResolver resolver = context.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources resources = context.getResources();
+
+        mSmartspace = (SwitchPreferenceCompat) findPreference(KEY_SMARTSPACE);
+        mSmartspace.setOnPreferenceChangeListener(this);
+
+        mWeather = (SwitchPreferenceCompat) findPreference(KEY_WEATHER);
+        mWeather.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
+
+        if (preference == mSmartspace) {
+            mSmartspace.setChecked((Boolean)newValue);
+            updateWeatherSettings();
+            SystemUtils.showSystemUiRestartDialog(context);
+            return true;
+        } else if (preference == mWeather) {
+            mWeather.setChecked((Boolean)newValue);
+            SystemUtils.showSystemUiRestartDialog(context);
+            return true;
+        }
         return false;
+    }
+
+    private void updateWeatherSettings() {
+        if (mWeather == null || mSmartspace == null) return;
+
+        boolean weatherEnabled = OmniJawsClient.get().isOmniJawsEnabled(getContext());
+        mWeather.setEnabled(!mSmartspace.isChecked() && weatherEnabled);
+        mWeather.setSummary(!mSmartspace.isChecked() && weatherEnabled
+                ? R.string.lockscreen_weather_summary
+                : R.string.lockscreen_weather_enabled_info);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateWeatherSettings();
     }
 
     @Override
