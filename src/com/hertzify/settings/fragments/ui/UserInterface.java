5d3138cc02ef968cae7f7e10505e492e574040e6
Pranav Praveen <72447760+TwistedVision518@users.noreply.github.com>
2026-04-18 03:26:04 +0000

    Hertzify: Add emoji styles
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- src/com/hertzify/settings/fragments/ui/UserInterface.java
+++ src/com/hertzify/settings/fragments/ui/UserInterface.java
@@ -9,6 +9,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.SystemProperties;
 
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
@@ -21,6 +22,9 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.hertzify.settings.preferences.SystemSettingListPreference;
+import com.hertzify.settings.utils.SystemUtils;
+
 import java.util.List;
 
 @SearchIndexable
@@ -28,6 +32,11 @@ public class UserInterface extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "UserInterface";
+    private static final String KEY_EMOJI_STYLE = "emoji_style";
+    private static final String PROP_EMOJI_STYLE = "persist.sys.emoji_style";
+    private static final String DEFAULT_EMOJI_STYLE = "android";
+
+    private SystemSettingListPreference mEmojiStylePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,12 +47,23 @@ public class UserInterface extends SettingsPreferenceFragment implements
         final ContentResolver resolver = context.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources resources = context.getResources();
+
+        mEmojiStylePref = findPreference(KEY_EMOJI_STYLE);
+        if (mEmojiStylePref != null) {
+            mEmojiStylePref.setValue(SystemProperties.get(PROP_EMOJI_STYLE, DEFAULT_EMOJI_STYLE));
+            mEmojiStylePref.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
+        if (preference == mEmojiStylePref) {
+            SystemProperties.set(PROP_EMOJI_STYLE, (String) newValue);
+            SystemUtils.showSystemRestartDialog(context);
+            return true;
+        }
         return false;
     }
 
